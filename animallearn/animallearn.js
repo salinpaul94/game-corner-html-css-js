82b4334ca48ba1204486a23a1c1c0eb33346a3b9
salinpaul valooran <salinpv@hotmail.com>
2023-09-17 13:29:28 +0530

    added logic to animallear.js

---
+++ animallearn/animallearn.js
@@ -0,0 +1,56 @@
+const main = document.querySelector("main");
+const voiceSelect = document.getElementById("voice");
+const textarea = document.getElementById("text");
+const readBtn = document.getElementById("read");
+const toggleBtn = document.getElementById("toogle");
+const closeBtn = document.getElementById("close");
+
+const data = [
+  {
+    image: "zanimacat.jpt",
+    text: "cat",
+  },
+];
+
+data.forEach(createBox);
+
+function createBox(item) {
+  const box = document.createElement("div");
+  const { image, text } = item;
+  box.classList.add("box");
+  box.innerHTML = `
+    <img src = "${image}" alt="${text}" />
+    <p class = "info">${text}</p>
+  `;
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
+  main.appendChild(box);
+}
+
+const message = new SpeechSynthesisUtterance();
+
+function setTextMessage(text) {
+  message.text = text;
+}
+
+function speakText() {
+  speechSynthesis.speak(message);
+}
+
+let voices = [];
+function getVoice() {
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voiceSelect.appendChild(option);
+  });
+}
+
+speechSynthesis.addEventListener("voicechanged", getVoice);
+getVoice();
