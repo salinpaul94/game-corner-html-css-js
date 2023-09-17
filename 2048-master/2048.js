52173da7259fc6b229f1b2ba150218d35949f90b
salinpaul valooran <salinpv@hotmail.com>
2023-09-18 01:28:02 +0530

    removing error

--- 2048-master/2048.js
+++ 2048-master/2048.js
@@ -19,8 +19,8 @@ var gameObj = {
 
   empty: function () {
     var emptyList = [];
-    for (var row = 0; cell < 4; cell++) {
-      for (var cell = 0; cell < 4; cell ++) {
+    for (var row = 0; row < 4; row++) {
+      for (var cell = 0; cell < 4; cell++) {
         if (this.stage[cell][row].boxObj == null) {
           emptyList.push(this.stage[cell][row]);
         }
@@ -28,156 +28,199 @@ var gameObj = {
     }
     return emptyList;
   },
-
-  newBox: fucntion () {
+  newBox: function () {
     var _this = this;
 
-    var box = fucntion (obj) {
+    var box = function (obj) {
       var num = Math.random() > 0.9 ? 4 : 2;
       this.value = num;
       this.parent = obj;
-      this.domObj = function () {
-        var domBox = document.createElement('spa');
+      this.domObj = (function () {
+        var domBox = document.createElement("span");
         domBox.innerText = num;
         domBox.textContent = num;
-        domBox.className = 'row' + obj.position[0] + ' ' + 'cell' + obj.position[1] + ' ' + 'num' + num;
-        var root = document.getElementById('stage');
+        domBox.className =
+          "row" +
+          obj.position[0] +
+          " " +
+          "cell" +
+          obj.position[1] +
+          " " +
+          "num" +
+          num;
+        var root = document.getElementById("stage");
         root.appendChild(domBox);
         return domBox;
-      }();
+      })();
       obj.boxObj = this;
-    }
-    var emtyList = this.empty();
+    };
+    var emptyList = this.empty();
     if (emptyList.length) {
-      var randomIndex = Math.floor(Math.random() + emptyList.length);
-      new box(emptlyList[randomIndex]);
+      var randomIndex = Math.floor(Math.random() * emptyList.length);
+      new box(emptyList[randomIndex]);
       return true;
     }
   },
-  isEnd:function () {
+  isEnd: function () {
     var emptyList = this.empty();
     if (!emptyList.length) {
-      for(var i=0; i<4; i++) {
-        for(var j=0; j<4; j++) {
-          var obj=this.stage[i][j];
-          var objLeft=(j==0)?{boxObj:{value:0}}:this.stage[i][j-1];
-          var objRight=(j==3)?{boxObj:{value:0}}:this.stage[i][j+1];
-          var objUp=(i==0)?{boxObj:{value:0}}:this.stage[i-1][j];
-          var objDown=(i==3)?{boxObj:{value:0}}:this.stage[i+1][j];
-          if(obj.boxObj.value==objLeft.boxObj.value
-            ||obj.boxObj.value==objDown.boxObj.value
-            ||obj.boxObj.value==objRight.boxObj.value
-            ||obj.boxObj.value==objUp.boxObj.value
-            ){
-              return false;
-            }
+      for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+          var obj = this.stage[i][j];
+          var objLeft =
+            j == 0 ? { boxObj: { value: 0 } } : this.stage[i][j - 1];
+          var objRight =
+            j == 3 ? { boxObj: { value: 0 } } : this.stage[i][j + 1];
+          var objUp = i == 0 ? { boxObj: { value: 0 } } : this.stage[i - 1][j];
+          var objDown =
+            i == 3 ? { boxObj: { value: 0 } } : this.stage[i + 1][j];
+          if (
+            obj.boxObj.value == objLeft.boxObj.value ||
+            obj.boxObj.value == objDown.boxObj.value ||
+            obj.boxObj.value == objRight.boxObj.value ||
+            obj.boxObj.value == objUp.boxObj.value
+          ) {
+            return false;
+          }
         }
       }
       return true;
     }
     return false;
   },
-  gameOver:function () {
-    alert('GAME OVER!');
+  gameOver: function () {
+    alert("GAVE OVER!");
   },
-  moveTo :function (obj1, obj2) {
+  moveTo: function (obj1, obj2) {
     obj2.boxObj = obj1.boxObj;
-    obj2.boxObj.domObj.className = 'row' + obj2.position[0] + ' ' + 'celi' + obj2.position[1] + ' ' + 'num' + obj2.boxObj.value;
+    obj2.boxObj.domObj.className =
+      "row" +
+      obj2.position[0] +
+      " " +
+      "cell" +
+      obj2.position[1] +
+      " " +
+      "num" +
+      obj2.boxObj.value;
 
     obj1.boxObj = null;
   },
-  addTo : function (obj1, obj2) {
+  addTo: function (obj1, obj2) {
     obj2.boxObj.domObj.parentNode.removeChild(obj2.boxObj.domObj);
     obj2.boxObj = obj1.boxObj;
     obj1.boxObj = null;
-    obj2.boxObj.value = obj2.boxObj.value + 2;
-    obj2.boxObj.domObj.className = 'row' + obj2.position[0] + ' ' + 'cell' + obj2.position[1] + ' ' + 'num' + obj2.boxObj.value;
+    obj2.boxObj.value = obj2.boxObj.value * 2;
+    obj2.boxObj.domObj.className =
+      "row" +
+      obj2.position[0] +
+      " " +
+      "cell" +
+      obj2.position[1] +
+      " " +
+      "num" +
+      obj2.boxObj.value;
     obj2.boxObj.domObj.innerText = obj2.boxObj.value;
     obj2.boxObj.domObj.textContent = obj2.boxObj.value;
     this.points.score += obj2.boxObj.value;
-    var scoreBar = document.getElementById('score');
+    var scoreBar = document.getElementById("score");
     scoreBar.innerText = this.points.score;
     scoreBar.textContent = this.points.score;
     return obj2.boxObj.value;
   },
-
-  clear:function(x,y){
-    var can=0;
-    for(var i=0;i<4;i++) {
-      var fst=null;
-      var fstEmpty=null;
-      for(var j=0;j<4;j++) {
-        var objInThisWay=null;
-        switch(""+x+y) {
-          case '00': objInThisWay=thsi.stage[i][j];break;
-          case '10': objInThisWay=this.stage[j][i];break;
-          case '11': objInThisWay=this.stage[3-j][i];break;
-          case '01': objInThisWay=this.stage[i][3-j];break;
+  clear: function (x, y) {
+    var can = 0;
+    for (var i = 0; i < 4; i++) {
+      var fst = null;
+      var fstEmpty = null;
+      for (var j = 0; j < 4; j++) {
+        var objInThisWay = null;
+        switch ("" + x + y) {
+          case "00":
+            objInThisWay = this.stage[i][j];
+            break;
+          case "10":
+            objInThisWay = this.stage[j][i];
+            break;
+          case "11":
+            objInThisWay = this.stage[3 - j][i];
+            break;
+          case "01":
+            objInThisWay = this.stage[i][3 - j];
+            break;
         }
-        if(objInThisWay.boxObj!=null) {
-          if(fstEmpty){
-            this.moveTo(objInThisWay,fstEmpty)
-            fstEmpty=null;
-            j=0;
-            can=1;
+        if (objInThisWay.boxObj != null) {
+          if (fstEmpty) {
+            this.moveTo(objInThisWay, fstEmpty);
+            fstEmpty = null;
+            j = 0;
+            can = 1;
           }
-        } else if(!fstEmpty){
-          fstEmpty=objInThisWay;
+        } else if (!fstEmpty) {
+          fstEmpty = objInThisWay;
         }
       }
     }
     return can;
   },
 
-  move: fucntion (x, y) {
-    var can=0;
-    can=this.clear(x,y)?1:0;
-    var add=0;
-    for(var i=0;i<4;i++){
-      for(var j=0;j<3;j++){
-        var objInThisWay=null;
-        var objInThisWay2=null;
-        switch (""+x+y){
-          case '00':{
-            objInThisWay=this.stage[i][j];
-            objInThisWay2=this.stage[i][j+1];break;
+  move: function (x, y) {
+    var can = 0;
+    can = this.clear(x, y) ? 1 : 0;
+    var add = 0;
+    for (var i = 0; i < 4; i++) {
+      for (var j = 0; j < 3; j++) {
+        var objInThisWay = null;
+        var objInThisWay2 = null;
+        switch ("" + x + y) {
+          case "00": {
+            objInThisWay = this.stage[i][j];
+            objInThisWay2 = this.stage[i][j + 1];
+            break;
           }
-          case '10':{
-            objInThisWay=this.stage[j][i];
-            objInThisWay2=this.stage[j+1][i];break;
+          case "10": {
+            objInThisWay = this.stage[j][i];
+            objInThisWay2 = this.stage[j + 1][i];
+            break;
           }
-          case '11':{
-            objInThisWay=this.stage[3-j][i];
-            objInThisWay2=this.stage[2-j][i];break;
+
+          case "11": {
+            objInThisWay = this.stage[3 - j][i];
+            objInThisWay2 = this.stage[2 - j][i];
+            break;
           }
-          case '01':{
-            objInThisWay=this.stage[i][3-j];
-            objInThisWay2=this.stage[i][2j];break;
+          case "01": {
+            objInThisWay = this.stage[i][3 - j];
+            objInThisWay2 = this.stage[i][2 - j];
+            break;
           }
         }
-        if(objInThisWay2.boxObj&&objInThisWay.boxObj.value==objInThisWay2.boxObj.value){
-          add+=this.addTo(objInThisWay2,objInThisWay);
-          this.clear(x,y);
+        if (
+          objInThisWay2.boxObj &&
+          objInThisWay.boxObj.value == objInThisWay2.boxObj.value
+        ) {
+          add += this.addTo(objInThisWay2, objInThisWay);
+          this.clear(x, y);
 
-          can=1;
+          can = 1;
         }
       }
     }
-    if(add){
-      var addscore=document.getElementById('addScore');
-      addscore.innerText="+"+add;
-      addscore.textContent="+"+add;
-      addscore.className="show";
-      setTimeout(function(){
-        addscore.className="hide";
-      },500);
+    if (add) {
+      var addscore = document.getElementById("addScore");
+      addscore.innerText = "+" + add;
+      addscore.textContent = "+" + add;
+      addscore.className = "show";
+      setTimeout(function () {
+        addscore.className = "hide";
+      }, 500);
     }
-    if(can){
+    if (can) {
       this.newBox();
     }
-    if(this.isEnd()){
+    if (this.isEnd()) {
       this.gameOver();
     }
   },
-  init: null
+
+  inti: null,
 };
