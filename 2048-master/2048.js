5f85aa00e2643abffa4cf3e2e0bf81f238adff18
salinpaul valooran <salinpv@hotmail.com>
2023-09-20 03:30:34 +0530

    created gameObj and initstage function and empty function

--- 2048-master/2048.js
+++ 2048-master/2048.js
@@ -1,5 +1,37 @@
+var gameObj = {
+  points: {
+    score: 0,
+    history: [],
+    status: 1,
+  },
+  stage: [],
+  intiStage: function () {
+    for (var cell = 0; cell < 4; cell++) {
+      this.stage[cell] = [];
+      for (var row = 0; row < 4; row++) {
+        this.stage[cell][row] = {
+          boxObj: null,
+          position: [cell, row],
+        };
+      }
+    }
+  },
+
+  empty: function () {
+    var emptyList = [];
+    for (var row = 0; row < 4; row++) {
+      for (var cell = 0; cell < 4; cell++) {
+        if (this.stage[cell][row].boxObj == null) {
+          emptyList.push(this.stage[cell][row]);
+        }
+      }
+    }
+    return emptyList;
+  },
+};
+
 window.onload = function () {
-  gameObj.initStage();
+  gameObj.intiStage();
   gameObj.newBox();
 
   var stage = document.getElementById("stage");
